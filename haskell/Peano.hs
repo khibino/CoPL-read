19bda261951a7bd75455c812db407287c07c0286
Kei Hibino <ex8k.hibino@gmail.com>
2015-12-29 16:00:33 +0900

    Drop unused pragmas.

--- haskell/Peano.hs
+++ haskell/Peano.hs
@@ -1,23 +1,12 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE UndecidableInstances #-}
 
 module Peano where
 
 
 data Nat = Z
          | S Nat deriving (Eq,Ord,Show,Read)
-
--- type family (m :: Nat) :+ (n :: Nat) :: Nat
--- type instance 'Z :+ n = n
--- type instance ('S m) :+ n = 'S (m :+ n)
-
--- type family (m :: Nat) :* (n :: Nat) :: Nat
--- type instance     'Z :* n = 'Z
--- type instance ('S m) :* n = (m :* n) :+ n
 
 data Nat' (n :: Nat) where
   Z'    :: Nat' 'Z
